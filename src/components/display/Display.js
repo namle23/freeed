cce32c1954548fc1d081d0dc64a36dda8497d463
namle23 <quangnamle23@gmail.com>
2020-01-14 17:09:32 +0200

    Change to socket.io client for polling

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,68 +1,37 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import './Display.css'
 import Current from '../current/Current'
 import History from '../history/History'
-
-let urlCurrent = 'https://evening-oasis-60569.herokuapp.com/current'
-let urlHistory = 'https://evening-oasis-60569.herokuapp.com/history'
-// let urlCurrent = 'http://localhost:8080/current'
-// let urlHistory = 'http://localhost:8080/history'
+import socketIOClient from 'socket.io-client'
 
 class Display extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      weatherData: null,
-      historyData: []
-    }
-  }
-
-  async getCurrentWeatherData() {
-    try {
-      return await axios.get(urlCurrent)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  async getHistoryData() {
-    try {
-      return await axios.get(urlHistory)
-    } catch (error) {
-      console.error(error)
+      weatherData: false,
+      historyData: [],
+      endpoint: 'https://evening-oasis-60569.herokuapp.com',
+      test: false
     }
   }
 
   componentDidMount() {
-    this.getCurrentWeatherData().then(weather => {
-      this.setState({ weatherData: weather.data })
-    })
-
-    this.getHistoryData().then(history => {
-      this.setState({ historyData: history.data })
-    })
-
-    setInterval(this.getCurrentWeatherData, 30000)
-    setInterval(this.getHistoryData, 30000)
-
-    setInterval(() => {
-      this.getCurrentWeatherData().then(weather => {
-        this.setState({ weatherData: weather.data })
-      })
-
-      this.getHistoryData().then(history => {
-        this.setState({ historyData: history.data })
-      })
-    }, 30000)
+    const { endpoint } = this.state
+    const socket = socketIOClient(endpoint)
+    socket.on('FromAPI', data => this.setState({ weatherData: data }))
+    socket.on('FromHistoryAPI', data => this.setState({ historyData: data }))
   }
 
   render() {
+    const { weatherData } = this.state
+
     return (
       <div>
         <Current weatherData={this.state.weatherData}></Current>
         <History historyData={this.state.historyData}></History>
+
+        {weatherData ? <section></section> : <i>Loading...</i>}
       </div>
     )
   }
